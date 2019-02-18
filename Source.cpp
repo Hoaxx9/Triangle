04e1c6e66bc837d696d84c6566e09c649c865139
Hoaxx9 <katerina.tikhonova_9@mail.ru>
2019-02-18 13:16:00 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,85 +1,10 @@
-/*
-*#include <glut.h>
-
-/*void renderScene(void) {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glBegin(GL_TRIANGLES);
-	glVertex3f(-0.5, -0.5, 0.0);
-	glVertex3f(0.0, 0.5, 0.0);
-	glVertex3f(0.5, -0.5, 0.0);
-	glEnd();
-
-
-	glutSwapBuffers();
-}
-
-int main(int argc, char **argv) {
-
-	// инициализация
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(400, 400);
-	glutCreateWindow("Урок 1");
-
-	// регистрация обратных вызовов
-	glutDisplayFunc(renderScene);
-
-	// Основной цикл GLUT
-	glutMainLoop();
-
-	return 1;
-}
-using namespace std;
-
-void Draw() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_LINES);
-	for (float i = -200.0; i <= 200.0; i += 40.0) {
-		glVertex2f(i, -200.0);
-		glVertex2f(i, 200.0);
-		glVertex2f(-200.0, i);
-		glVertex2f(200.0, i);
-	}
-	glEnd();												//только для GLUT_SINGLE
-
-	glFlush();
-}
-void Initialize() {
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-200.0, 200.0, -200.0, 200.0, -200.0, 200.0);	//выяснить накуя
-
-	glMatrixMode(GL_MODELVIEW);
-	glTranslatef(-50.0, 50.0, 0.0);
-	glRotatef(20, 0.0, 0.0, 1.0);
-	glScalef(0.6, 0.6, 1.0);
-}
-
-int main(int argc, char **argv) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-
-	glutInitWindowPosition(100, 200);
-	glutInitWindowSize(400, 400);
-	glutCreateWindow("Lesson 1");
-
-	glutDisplayFunc(Draw);
-	Initialize();
-
-	glutMainLoop();
-	return 0;
-}*/
 #include <stdlib.h>
 #include <glut.h>
 
 float red = 1.0f, blue = 1.0f, green = 1.0f;
 float angle = 0.0f;
 
-void changeSize(int w, int h) {
+void changeSize(int w, int h) {					//РѕР±СЂР°Р±РѕС‚РєР° РёР·РјРµРЅРµРЅРёСЏ СЂР°Р·РјРµСЂРѕРІ РѕРєРЅР° РїСЂРѕРіСЂР°РјРјС‹
 	if (h == 0)
 		h = 1;
 	float ratio = w * 1.0 / h;
@@ -91,7 +16,7 @@ void changeSize(int w, int h) {
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void renderScene(void) {									//отрисовка кадра
+void renderScene(void) {					//РѕС‚СЂРёСЃРѕРІРєР° РєР°РґСЂР°
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 	gluLookAt(0.0f, 0.0f, 10.0f,
@@ -112,12 +37,12 @@ void renderScene(void) {									//отрисовка кадра
 	glutSwapBuffers();
 }
 
-void processNormalKeys(unsigned char key, int x, int y) {	//закрытие окна программы по нажатию Esc
+void processNormalKeys(unsigned char key, int x, int y) {	//Р·Р°РєСЂС‹С‚РёРµ РѕРєРЅР° РїСЂРѕРіСЂР°РјРјС‹ РїРѕ РЅР°Р¶Р°С‚РёСЋ Esc
 	if (key == 27)
 		exit(0);
 }
 
-void processSpecialKeys(int key, int x, int y) {			//обработка команд с клавиатуры
+void processSpecialKeys(int key, int x, int y) {		//РѕР±СЂР°Р±РѕС‚РєР° РєРѕРјР°РЅРґ СЃ РєР»Р°РІРёР°С‚СѓСЂС‹
 	switch (key) {
 	case GLUT_KEY_F1:
 		red = 1.0;
@@ -139,7 +64,7 @@ int main(int argc, char **argv) {
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(400, 400);
-	glutCreateWindow("Урок 4");
+	glutCreateWindow("Г“Г°Г®ГЄ 4");
 
 	glutDisplayFunc(renderScene);
 	glutReshapeFunc(changeSize);
